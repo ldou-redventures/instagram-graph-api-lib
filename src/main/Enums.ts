debb75057d72326885d393ed2d1a50fc1bba9d30
Lin Dou <80337828+ldou-redventures@users.noreply.github.com>
2022-10-10 13:41:29 -0700

    update api version to 15

--- src/main/Enums.ts
+++ src/main/Enums.ts
@@ -577,6 +577,16 @@ export enum ApiVersion {
      * {@link https://developers.facebook.com/docs/graph-api/changelog/version13.0}
      */
     V13_0 = 'v13.0',
+    
+    /**
+     * {@link https://developers.facebook.com/docs/graph-api/changelog/version14.0}
+     */
+    V14_0 = 'v14.0',
+    
+     /**
+     * {@link https://developers.facebook.com/docs/graph-api/changelog/version15.0}
+     */
+    V15_0 = 'v15.0',
 
-    LATEST = V13_0,
+    LATEST = V15_0,
 }
